e5d40be205841a1447dbbf3d56b34112051f7a16
RajaLufhti <rajamuh2709@gmail.com>
2025-11-18 15:04:55 +0700

    Add files via upload

---
+++ Pertemuan7_Modul7/UNGUIDED02/stack.cpp
@@ -0,0 +1,55 @@
+#include "stack.h"
+#include <iostream>
+using namespace std;
+
+void createStack(Stack &S) {
+    S.top = -1;
+}
+
+void push(Stack &S, infotype x) {
+    if (S.top < 19) {
+        S.top++;
+        S.info[S.top] = x;
+    }
+}
+
+infotype pop(Stack &S) {
+    if (S.top >= 0) {
+        infotype x = S.info[S.top];
+        S.top--;
+        return x;
+    }
+    return -1; 
+}
+
+void printInfo(Stack S) {
+    cout << "[TOP] ";
+    for (int i = S.top; i >= 0; i--) {
+        cout << S.info[i] << " ";
+    }
+    cout << endl;
+}
+
+void balikStack(Stack &S) {
+    Stack temp;
+    createStack(temp);
+    while (S.top >= 0) {
+        push(temp, pop(S));
+    }
+    S = temp;
+}
+
+void pushAscending(Stack &S, infotype x) {
+    Stack temp;
+    createStack(temp);
+
+    while (S.top >= 0 && S.info[S.top] > x) {
+        push(temp, pop(S));
+    }
+
+    push(S, x);
+
+    while (temp.top >= 0) {
+        push(S, pop(temp));
+    }
+}
